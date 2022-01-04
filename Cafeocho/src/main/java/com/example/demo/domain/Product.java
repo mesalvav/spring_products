22caa39a7a3bd6274cc75cc836db7c7679d99e8e
mesalvav@yahoo.com <msalv006@fiu.edu>
2022-01-04 17:55:53 -0500

    added jpa h2 and made product entity

--- Cafeocho/src/main/java/com/example/demo/domain/Product.java
+++ Cafeocho/src/main/java/com/example/demo/domain/Product.java
@@ -1,9 +1,18 @@
 package com.example.demo.domain;
 
 import java.math.BigDecimal;
+import javax.persistence.*;
 
-public class Product implements DomainObject {
+@Entity
+public class Product implements DomainObject{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Version
+    private Integer version;
+
     private String description;
     private BigDecimal price;
     private String imageUrl;
@@ -14,6 +23,14 @@ public class Product implements DomainObject {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
     }
 
     public String getDescription() {
@@ -40,4 +57,43 @@ public class Product implements DomainObject {
         this.imageUrl = imageUrl;
     }
 }
+//
+//public class Product implements DomainObject {
+//    private Integer id;
+//    private String description;
+//    private BigDecimal price;
+//    private String imageUrl;
+//
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public BigDecimal getPrice() {
+//        return price;
+//    }
+//
+//    public void setPrice(BigDecimal price) {
+//        this.price = price;
+//    }
+//
+//    public String getImageUrl() {
+//        return imageUrl;
+//    }
+//
+//    public void setImageUrl(String imageUrl) {
+//        this.imageUrl = imageUrl;
+//    }
+//}
 
