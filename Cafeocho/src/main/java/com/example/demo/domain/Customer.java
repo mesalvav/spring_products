c562fa68fb4f108d6d94e54982c2b0bb3cc910b1
mesalvav@yahoo.com <msalv006@fiu.edu>
2022-01-04 18:25:32 -0500

    added customer entity to h2

--- Cafeocho/src/main/java/com/example/demo/domain/Customer.java
+++ Cafeocho/src/main/java/com/example/demo/domain/Customer.java
@@ -1,8 +1,18 @@
 package com.example.demo.domain;
 
+import javax.persistence.*;
+
+
+@Entity
 public class Customer implements DomainObject {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Version
+    private Integer version;
+
     private String firstName;
     private String lastName;
     private String email;
@@ -21,6 +31,14 @@ public class Customer implements DomainObject {
     @Override
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
     }
 
     public String getFirstName() {
@@ -95,3 +113,99 @@ public class Customer implements DomainObject {
         this.zipCode = zipCode;
     }
 }
+
+//public class Customer implements DomainObject {
+//
+//    private Integer id;
+//    private String firstName;
+//    private String lastName;
+//    private String email;
+//    private String phoneNumber;
+//    private String addressLine1;
+//    private String addressLine2;
+//    private String city;
+//    private String state;
+//    private String zipCode;
+//
+//    @Override
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    @Override
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPhoneNumber() {
+//        return phoneNumber;
+//    }
+//
+//    public void setPhoneNumber(String phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
+//
+//    public String getAddressLine1() {
+//        return addressLine1;
+//    }
+//
+//    public void setAddressLine1(String addressLine1) {
+//        this.addressLine1 = addressLine1;
+//    }
+//
+//    public String getAddressLine2() {
+//        return addressLine2;
+//    }
+//
+//    public void setAddressLine2(String addressLine2) {
+//        this.addressLine2 = addressLine2;
+//    }
+//
+//    public String getCity() {
+//        return city;
+//    }
+//
+//    public void setCity(String city) {
+//        this.city = city;
+//    }
+//
+//    public String getState() {
+//        return state;
+//    }
+//
+//    public void setState(String state) {
+//        this.state = state;
+//    }
+//
+//    public String getZipCode() {
+//        return zipCode;
+//    }
+//
+//    public void setZipCode(String zipCode) {
+//        this.zipCode = zipCode;
+//    }
+//}
